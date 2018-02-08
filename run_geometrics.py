a0de803d04df02257a57d869f448b8c45d357e33
Andrea <andrea.leichtman.jhuapl.edu>
2018-02-08 09:48:38 -0500

    Fixed default terrain threshold to be 1m

--- run_geometrics.py
+++ run_geometrics.py
@@ -170,7 +170,7 @@ def run_geometrics(configfile,refpath=None,testpath=None,outputpath=None,align=T
     try:
         dtm_z_threshold = config['OPTIONS']['TerrainZErrorThreshold']
     except:
-        dtm_z_threshold = unitHgt
+        dtm_z_threshold = 1
 
     metrics['terrain_accuracy'] = geo.run_terrain_accuracy_metrics(refDTM, testDTM, refMask, testMask, dtm_z_threshold, geo.getUnitArea(tform), plot=plot)
 
