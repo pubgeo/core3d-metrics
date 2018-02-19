89186794699ed39888a9f54dcd6a1d23997bf43d
Andrea <andrea.leichtman.jhuapl.edu>
2018-02-19 07:18:59 -0500

    Fixed whitespace

--- run_geometrics.py
+++ run_geometrics.py
@@ -162,7 +162,7 @@ def run_geometrics(configfile,refpath=None,testpath=None,outputpath=None,align=T
     metrics['registration_offset'] = xyzOffset
     # Run the terrain model metrics and report results.
     
-	dtm_z_threshold = config['OPTIONS'].get('TerrainZErrorThreshold',1)
+    dtm_z_threshold = config['OPTIONS'].get('TerrainZErrorThreshold',1)
     metrics['terrain_accuracy'] = geo.run_terrain_accuracy_metrics(refDTM, testDTM, refMask, testMask, dtm_z_threshold, geo.getUnitArea(tform), plot=plot)
 
     metrics['relative_accuracy'] = geo.run_relative_accuracy_metrics(refDSM, testDSM, refMask, testMask, plot=plot)
